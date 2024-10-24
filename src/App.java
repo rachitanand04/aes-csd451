9b4f91ecf2dd658731e21f8f7e070efc0032d959
rachit <rachit.anand04111@outlook.com>
2024-10-24 13:27:33 +0530

    added binary conversion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String binary = toBinaryString(input);
+        String text = toTexString(binary);
+        System.out.println(text);
+        scanner.close();
+    }
+
+    public static String toBinaryString(String message){
+        byte[] bytes = message.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for(byte b : bytes){
+            int val = b;
+            for(int i = 0; i < 8; i++){
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary.toString();
+    }
+
+    public static String toTexString(String message){
+        StringBuilder text = new StringBuilder();
+        for(int i = 0; i < message.length(); i=i+8){
+            int charCode = Integer.parseInt(message.substring(i, i+8),2);
+            text.append((char)charCode);
+        }
+        return text.toString();
     }
 }
