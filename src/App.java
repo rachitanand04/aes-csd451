280dd758e85b56d07d6f71cdbb23eae6a5507625
rachit <rachit.anand04111@outlook.com>
2024-11-02 16:09:12 +0530

    added subsitution and shifting

--- src/App.java
+++ src/App.java
@@ -1,13 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String binary = toBinaryString(input);
-        String text = toTexString(binary);
-        System.out.println(text);
         scanner.close();
+        String message = "absentmindedness";
+        byte[] bytes = message.getBytes();
+        String[][] hexStrings = new String[4][4];
+        int count = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                hexStrings[i][j] = Integer.toHexString(bytes[count]);
+                count++;
+            }
+        }
+        for(String[] strings: hexStrings){
+            System.out.println(Arrays.toString(strings));
+        }
+        System.out.println();
+        performSubstitution(hexStrings);
+        for(String[] strings: hexStrings){
+            System.out.println(Arrays.toString(strings));
+        }
+        System.out.println();
+        shiftRows(hexStrings);
+        for(String[] strings: hexStrings){
+            System.out.println(Arrays.toString(strings));
+        }
+
     }
 
     public static String toBinaryString(String message){
@@ -30,5 +51,31 @@ public class App {
             text.append((char)charCode);
         }
         return text.toString();
+    }
+    
+    public static void performSubstitution(String[][] array){
+        SBox sBox = new SBox();
+        // String[][] outArray = new String[4][4];
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                array[i][j] = sBox.getSBoxValue(array[i][j]);
+            }
+        }
+    }
+
+    public static void shiftRows(String[][] array){
+        for(int i = 0; i < 4; i++){
+            rotate(array[i], i);
+        }
+    }
+
+    public static void rotate(String[] array, int n){
+        String[] temp = new String[4];
+        for(int i = 0; i < 4; i++){
+            temp[i] = array[i];
+        }
+        for(int i = 0; i < 4; i++){
+            array[i] = temp[(i+n)%4];
+        }
     }
 }
