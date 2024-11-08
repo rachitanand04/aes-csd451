04bb307ef5613b2268dbe3faadeb44973132420e
rachit <rachit.anand04111@outlook.com>
2024-11-08 22:38:20 +0530

    Done!!

--- bin/bytegen.py
+++
@@ -1,8 +0,0 @@
-import zlib
-
-# Replace 'data' with the bytes of the data you want to checksum
-data = b""
-
-# Calculate CRC32
-checksum = zlib.crc32(data) & 0xFFFFFFFF  # Mask to 32-bits
-print(f"CRC32 Checksum: {checksum:08X}")
